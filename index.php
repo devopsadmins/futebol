49905a7ad5d6e4752b2e71d208cc849c66072312
Everton Lima <eolimabr@gmail.com>
2022-06-06 23:11:19 -0300

    Sem Vendor - composer update

--- index.php
+++ index.php
@@ -11,13 +11,16 @@ if ($res->getStatusCode() === 200){
     $games = json_decode($res->getBody());
 };
 $league = "Corinthians";
+$lastGames = 5;
 $results = [];
+$lastResults = []; 
 
 foreach ($games->JOGOS as $game) {
     if (($game->MANDANTE == $league || $game->VISITANTE == $league) && $game->STATUS_JOGO=='finalizado' ) {
         $results[] = $game;
     }
 }
-for ($a = count($results) - 1; $a >= count($results)-5; $a--) {
-    print_r($results[$a]);
+for ($a = count($results) - 1; $a >= count($results)-lastGames; $a--) {
+    $lastResults[] = $results[$a];
 }
+print_r($lastResults);
